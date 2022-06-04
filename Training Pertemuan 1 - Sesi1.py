24f5202526bd9537b7a1640e8f2019a7b7646ae8
adibudiarto <adibudiart@gmail.com>
2022-06-04 13:43:48 +0700

    Initial Commit add Sesi 1

---
+++ Training Pertemuan 1 - Sesi1.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+
+print(10)
+print(type(10))
+
+
+# In[2]:
+
+
+print(4.2)
+print(type(4.2))
+
+print(4.)
+
+print(.2)
+
+print(.4e7)
+
+print(4.2e-4)
+
+
+# In[3]:
+
+
+print(4.2)
+print(type(4.2))
+
+print(4.)
+
+print(.2)
+
+print(.4e7)
+
+print(4.2e-4)
+
+
+# In[4]:
+
+
+print(4.2)
+
+
+# In[5]:
+
+
+print(type(4.2))
+
+
+# In[8]:
+
+
+print (type('cobacoba'))
+
+
+# In[11]:
+
+
+print (type(Trwue))
+
+
+# In[12]:
+
+
+n = 300
+print (n)
+
+
+# In[13]:
+
+
+n = 300 ; b = 500
+print (n * b)
+
+
+# In[16]:
+
+
+a = 10 ; t = 20
+l = a * t / 2
+print (l)
+
+
+# In[17]:
+
+
+a = b = c = 100
+print (a,b,c)
+
+
+# In[18]:
+
+
+var = 23.5
+print(var)
+
+var = "Now I'm a string"
+print(var)
+
+
+# In[22]:
+
+
+a = 10 ; b = 20
+a+b
+
+
+# In[23]:
+
+
+s = 'foo'
+t = 'bar'
+u = 'baz'
+
+print(s + t)
+
+print(s + t + u)
+
+
+print('Hacktiv8 ' + 'PTP')
+
+
+# In[ ]:
+
+
+
+
